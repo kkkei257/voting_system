899d8d7d34f73eee3c6870e45f580c7121e4adcc
kkkei257 <takanic.gear@gmail.com>
2019-08-27 00:27:00 +0900

    change index, script, css

--- vote_script.js
+++ vote_script.js
@@ -16,128 +16,196 @@ var list = ['佐藤','Satou','satou','さとう',
             '渡辺','Watanabe','watanabe','わたなべ',
             '伊藤','Itou','itou','いとう',
             '山本','Yamamoto','yamamoto','やまもと',
-            '中村','Nakamura','nakamura','なかむら',
-            '加瀬','Kase','kase','かせ']
+            '中村','Nakamura','nakamura','なかむら'];
 
 //PとFGの人数
-var p_flag = 2;
-var fg_flag = 2;
+var p_flag = 1;
+var fg_flag = 1;
+
 
 
 //各入力フォームに変化があった場合にそれぞれの関数が実行される
 //正しい名前かどうかの確認
+/*
 function submit_p1(){
-  var p_1 = document.getElementById("p_1").value;
+  var p1 = document.getElementById("p1").value;
       
-  if(list.indexOf(p_1) >= 0){
-    document.getElementById("p_1").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(p1) >= 0){
+    document.getElementById("p1").style.backgroundColor = "#FFFFFF";
     p1_flag = 1;
     return true;
   }else{
     //alert(p_1 + 'はリストに存在しません.');
-    document.getElementById("p_1").style.backgroundColor = "mistyrose";
+    document.getElementById("p1").style.backgroundColor = "mistyrose";
     p1_flag = 0;
     return false;
   }
 }
 
 function submit_p2(){
-  var p_2 = document.getElementById("p_2").value;
+  var p2 = document.getElementById("p2").value;
       
-  if(list.indexOf(p_2) >= 0){
-    document.getElementById("p_2").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(p2) >= 0){
+    document.getElementById("p2").style.backgroundColor = "#FFFFFF";
     p2_flag = 1;
     return true;
   }else{
-    document.getElementById("p_2").style.backgroundColor = "mistyrose";
+    document.getElementById("p2").style.backgroundColor = "mistyrose";
     p2_flag = 0;
     return false;
   }
 }
 
 function submit_p3(){
-  var p_3 = document.getElementById("p_3").value;
+  var p3 = document.getElementById("p3").value;
       
-  if(list.indexOf(p_3) >= 0){
-    document.getElementById("p_3").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(p3) >= 0){
+    document.getElementById("p3").style.backgroundColor = "#FFFFFF";
     p3_flag = 1;
     return true;
   }else{
-    document.getElementById("p_3").style.backgroundColor = "mistyrose";
+    document.getElementById("p3").style.backgroundColor = "mistyrose";
     p3_flag = 0;
     return false;
   }
 }
 
 function submit_p4(){
-  var p_4 = document.getElementById("p_4").value;
+  var p4 = document.getElementById("p4").value;
       
-  if(list.indexOf(p_4) >= 0){
-    document.getElementById("p_4").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(p4) >= 0){
+    document.getElementById("p4").style.backgroundColor = "#FFFFFF";
     p4_flag = 1;
     return true;
   }else{
-    document.getElementById("p_4").style.backgroundColor = "mistyrose";
+    document.getElementById("p4").style.backgroundColor = "mistyrose";
     p4_flag = 0;
     return false;
   }
 }
 
 function submit_fg1(){
-  var fg_1 = document.getElementById("fg_1").value;
+  var fg1 = document.getElementById("fg1").value;
       
-  if(list.indexOf(fg_1) >= 0){
-    document.getElementById("fg_1").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(fg1) >= 0){
+    document.getElementById("fg1").style.backgroundColor = "#FFFFFF";
     fg1_flag = 1;
     return true;
   }else{
-    document.getElementById("fg_1").style.backgroundColor = "mistyrose";
+    document.getElementById("fg1").style.backgroundColor = "mistyrose";
     fg1_flag = 0;
     return false;
   }
 }
 
 function submit_fg2(){
-  var fg_2 = document.getElementById("fg_2").value;
+  var fg2 = document.getElementById("fg2").value;
       
-  if(list.indexOf(fg_2) >= 0){
-    document.getElementById("fg_2").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(fg2) >= 0){
+    document.getElementById("fg2").style.backgroundColor = "#FFFFFF";
     fg2_flag = 1;
     return true;
   }else{
-    document.getElementById("fg_2").style.backgroundColor = "mistyrose";
+    document.getElementById("fg2").style.backgroundColor = "mistyrose";
     fg2_flag = 0;
     return false;
   }
 }
 
 function submit_fg3(){
-  var fg_3 = document.getElementById("fg_3").value;
+  var fg3 = document.getElementById("fg3").value;
       
-  if(list.indexOf(fg_3) >= 0){
-    document.getElementById("fg_3").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(fg3) >= 0){
+    document.getElementById("fg3").style.backgroundColor = "#FFFFFF";
     fg3_flag = 1;
     return true;
   }else{
-    document.getElementById("fg_3").style.backgroundColor = "mistyrose";
+    document.getElementById("fg3").style.backgroundColor = "mistyrose";
     fg3_flag = 0;
     return false;
   }
 }
 
 function submit_fg4(){
-  var fg_4 = document.getElementById("fg_4").value;
+  var fg4 = document.getElementById("fg4").value;
       
-  if(list.indexOf(fg_4) >= 0){
-    document.getElementById("fg_4").style.backgroundColor = "#FFFFFF";
+  if(list.indexOf(fg4) >= 0){
+    document.getElementById("fg4").style.backgroundColor = "#FFFFFF";
     fg4_flag = 1;
     return true;
   }else{
-    document.getElementById("fg_4").style.backgroundColor = "mistyrose";
+    document.getElementById("fg4").style.backgroundColor = "mistyrose";
     fg4_flag = 0;
     return false;
   }
 }
+*/
+
+
+function submit(thisId){
+  var value = document.getElementById(thisId).value;
+  
+  if(list.indexOf(value) >= 0){
+    document.getElementById(thisId).style.backgroundColor = "#FFFFFF";
+    switch(thisId){
+      case "p1":
+        p1_flag = 1;
+        break;
+      case "p2":
+        p2_flag = 1;
+        break;
+      case "p3":
+        p3_flag = 1;
+        break;
+      case "p4":
+        p4_flag = 1;
+        break;
+      case "fg1":
+        fg1_flag = 1;
+        break;
+      case "fg2":
+        fg2_flag = 1;
+        break;
+      case "fg3":
+        fg3_flag = 1;
+        break;
+      case "fg4":
+        fg4_flag = 1;
+        break;
+    }
+    return true;
+  }else{
+    document.getElementById(thisId).style.backgroundColor = "mistyrose";
+    switch(thisId){
+      case "p1":
+        p1_flag = 0;
+        break;
+      case "p2":
+        p2_flag = 0;
+        break;
+      case "p3":
+        p3_flag = 0;
+        break;
+      case "p4":
+        p4_flag = 0;
+        break;
+      case "fg1":
+        fg1_flag = 0;
+        break;
+      case "fg2":
+        fg2_flag = 0;
+        break;
+      case "fg3":
+        fg3_flag = 0;
+        break;
+      case "fg4":
+        fg4_flag = 0;
+        break;
+    }
+    return false;
+  }
+}
+
 
 
 // チェックボックスを全てチェックすると入力フォームがabledになる
@@ -207,11 +275,12 @@ setInterval(p_checkbox_check,1000);
 setInterval(fg_checkbox_check,1000);
 
 
+
 // 入力した名前が全て正しい&全てチェックしたらボタンを押せるようにする
 // 1秒ごとに入力フォームとチェックボックスの状態を確認
 function flag_check(){
   var check_list = new Array(8);
-  
+
   check_list[0] = document.getElementById("check_p1").checked;
   check_list[1] = document.getElementById("check_p2").checked;
   check_list[2] = document.getElementById("check_p3").checked;
@@ -231,7 +300,6 @@ function flag_check(){
     }
   }
   
-  
   if(check_flag == 0 && 
      (p1_flag + p2_flag + p3_flag + p4_flag == p_flag) &&
      (fg1_flag + fg2_flag + fg3_flag + fg4_flag == fg_flag)){
@@ -243,16 +311,16 @@ function flag_check(){
 setInterval(flag_check,1000);
 
 
+
 // 投票ボタンを押した時の処理
 function btn_send(){  
-  var check_list = new Array(8);
+  /*var check_list = new Array(8);
   
   check_list[0] = document.getElementById("check_p1").checked;
   check_list[1] = document.getElementById("check_p2").checked;
   check_list[2] = document.getElementById("check_p3").checked;
   check_list[3] = document.getElementById("check_p4").checked;
   check_list[4] = document.getElementById("check_p5").checked;
-  //check_list[5] = document.getElementById("check_p6").checked;
   check_list[5] = document.getElementById("check_fg1").checked;
   check_list[6] = document.getElementById("check_fg2").checked;
   check_list[7] = document.getElementById("check_fg3").checked;
@@ -273,10 +341,88 @@ function btn_send(){
   if(check_flag == 0){
     window.location.href = "result.html";
   }
+  */
+  window.location.href = "result.html";
 }
 
 
-//ここから下追加しました
+function predict(thisInput,thisUl){
+  const input = document.getElementById(thisInput);
+  const ul = document.getElementById(thisUl);
+  const dictMap = new Map([
+    ["佐藤", ["satou", "Satou","佐藤","さとう"]],
+    ["鈴木", ["suzuki", "Suzuki", "鈴木", "すずき"]],
+    ["高橋", ["takahashi", "Takahashi", "高橋", "たかはし"]],
+    ["田中", ["tanaka", "Tanaka", "田中", "たなか"]],
+    ["渡辺", ["watanabe", "Watanabe", "渡辺", "わたなべ"]],
+    ["伊藤", ["itou", "Itou", "伊藤", "いとう"]],
+    ["山本", ["yamamoto", "Yamamoto", "山本", "やまもと"]],
+    ["中村", ["nakamura", "Nakamura", "中村", "なかむら"]],
+  ]);
+  
+  
+  dictMap.forEach((v, key) => {
+    
+    const li = document.createElement("li");
+    li.innerHTML = key;
+    li.style.display = "none";
+    
+    // 候補部分をクリックした時
+    li.onclick = () => {
+      // 1回目の処理
+      input.value = li.innerHTML;
+      submit(thisInput);
+      for (let i = 0; i < ul.children.length; ++i) {
+        ul.children[i].style.display = "none";
+      }
+      
+      // 2回目以降
+      input.onblur = () => {
+        window.setTimeout(
+          (function(){
+            for (let i = 0; i < ul.children.length; ++i) {
+              ul.children[i].style.display = "none";
+            }
+          })
+        ,200);
+        window.setTimeout((function(){submit(thisInput)}),100);
+      };
+      
+    };
+    ul.appendChild(li);
+  });
+  
+  // フォームに文字を入力した時
+  input.onkeyup = () => {
+    const str = input.value;
+    let idx = 0;
+    dictMap.forEach((arr, key) => {
+      if (
+        str !== ""
+        && arr.reduce((acc, cur) => acc || cur.indexOf(str) === 0, false)
+      ) {
+        ul.children[idx].style.display = "block";
+      } else {
+        ul.children[idx].style.display = "none";
+      }
+      ++idx;
+    });
+    
+    input.onblur = () => {
+        window.setTimeout(
+          ()=>{
+            for (let i = 0; i < ul.children.length; ++i) {
+              ul.children[i].style.display = "none";
+            }
+          }
+        ,200);
+        window.setTimeout(()=>{submit(thisInput)},100);
+      };
+    
+  };
+}
+
+/*
 function addValue(thisId){
   let input_sent;//中のtext格納する変数
   //console.log(thisId.id)
@@ -296,17 +442,31 @@ function addValue(thisId){
   //console.log(input_sent);
   //これが予測変換用の辞書
   const names={
-               'satou':'Satou',
-               'suzuki':'Suzuki', 
-               'takahashi':'Takahashi', 
-               'tanaka':'Tanaka',
-               'watanabe':'Watanabe',
-               'itou':'Itou',
-               'yamamoto':'Yamamoto',
-               'nakamura':'Nakamura',
-               'kase':'Kase',
+               'seki':'Seki',
+               'mochida':'Mochida', 
+               'kase':'Kase', 
+               'chou':'Chou',
+               'you':'You',
+               'koyama':'Koyama',
+               'ghita':'Ghita',
+               'ogino':'Ogino',
+               'ikeda':'Ikeda',
+               'sakamoto':'Sakamoto',
+               'maemoto':'Maemoto',
+               'Seki':'Seki',
+               'Mochida':'Mochida', 
+               'Kase':'Kase', 
+               'Chou':'Chou',
+               'You':'You',
+               'Koyama':'Koyama',
+               'Ghita':'Ghita',
+               'Ogino':'Ogino',
+               'Ikeda':'Ikeda',
+               'Sakamoto':'Sakamoto',
+               'Maemoto':'Maemoto',
               }
               
+  //const names = ['関', '持田', '加瀬', '張','姚','小山','Ghita','荻野','池田','坂元','前本','せき','もちだ'];
   //配列内の単語でfor
   for(let key in names){
     //もしも、配列内の要素の一番初めの要素が合致していた場合(現段階では先頭要素のみ←と思っていたが、後ろ要素はautocompleteが自動的に見てくれているので実質全体見れている)
@@ -327,3 +487,4 @@ function addValue(thisId){
   }
   
 }
+*/
